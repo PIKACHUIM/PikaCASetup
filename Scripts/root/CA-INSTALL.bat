1fe6887f1f07452173086e986c17103fb993bd03
pikachuren <pikachuren@tencent.com>
2025-04-01 12:49:48 +0800

    fix

--- Scripts/root/CA-INSTALL.bat
+++ Scripts/root/CA-INSTALL.bat
@@ -13,16 +13,15 @@ echo             ##################################
 echo.
 echo.
 regedit.exe /s .\CA-INSTALL.reg
-certmgr.exe -crl -add /all .\CA-ECCP521.crl -s -r localMachine AuthRoot
-certmgr.exe -crl -add /all .\CA-RSA4096.crl -s -r localMachine AuthRoot
-certmgr.exe -crl -add /all .\CA-RSA8192.crl -s -r localMachine AuthRoot
-
+certmgr.exe -crl -add /all .\CA-ECCP521.crl -s -r localMachine Root
+certmgr.exe -crl -add /all .\CA-RSA4096.crl -s -r localMachine Root
+certmgr.exe -crl -add /all .\CA-RSA8192.crl -s -r localMachine Root
+certmgr.exe -crl -add /all .\CA-ECCP521.crl -s -r currentUser  Root
+certmgr.exe -crl -add /all .\CA-RSA4096.crl -s -r currentUser  Root
+certmgr.exe -crl -add /all .\CA-RSA8192.crl -s -r currentUser  Root
 certmgr.exe -add      /all .\CA-ECCP521.crt -s -r localMachine Root
 certmgr.exe -add      /all .\CA-RSA8192.crt -s -r localMachine Root
 certmgr.exe -add      /all .\CA-RSA4096.crt -s -r localMachine Root
-certmgr.exe -add      /all .\CA-ECCP521.crt -s -r currentUser  Root
-certmgr.exe -add      /all .\CA-RSA8192.crt -s -r currentUser  Root
-certmgr.exe -add      /all .\CA-RSA4096.crt -s -r currentUser  Root
 cls
 powershell Import-Certificate -FilePath .\CA-ECCP521.crt -CertStoreLocation Cert:\\LocalMachine\\Root
 powershell Import-Certificate -FilePath .\CA-RSA8192.crt -CertStoreLocation Cert:\\LocalMachine\\Root
